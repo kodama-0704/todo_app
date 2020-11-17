9c2f601972df12413f67797d648e78ad8cac7f64
kodama <risb180203@yahoo.co.jp>
2020-11-17 18:27:27 +0900

    finish 2-13

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,6 @@
 class TodosController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_goal
   before_action :set_todo, only: [:show, :edit, :update, :destroy, :sort]
 
   # GET /todos/new
